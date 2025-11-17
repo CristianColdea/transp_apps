9420f756e06f02c6a1748086ad1feadab1072427
Cristian Coldea <rdt333@gmail.com>
2025-11-17 09:27:43 +0200

    code update ...

--- nwcm.py
+++ nwcm.py
@@ -5,5 +5,5 @@ It takes as inputs the cost matrix, the supply and demand lists. The length of
 supply must match the number of cost matrix rows and that of demand the
 columns, respectively.
 Returns the allocation table, wether the feasible solution is possible, i.e.,
-bool and the allocation total cost.
+bool, and the allocation total cost.
 """
