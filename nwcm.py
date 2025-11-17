fd2902ff0e6aa6340adadb8b4f1096a51ba227fa
Cristian <rdt333@gmail.com>
2025-11-17 19:31:26 +0200

    core part coded, working fine

--- nwcm.py
+++ nwcm.py
@@ -16,9 +16,32 @@ c = [[2, 5, 1],
 
 s = [25, 23, 26]
 d = [31, 22, 21]
+sum_s = sum(s)
+sum_d = sum(d)
 
 assert len(c) == len(s)   #rows must be = to number of supply sources
 assert len(c[0]) == len(d)    #cols must be = to number of demands
 assert sum(s) == sum(d)    #sum of supply must be = to sum of demand
+
+# create a matrix of zeros for decision variables
+zrs = [ [0] * len(c[0]) for _ in range(len(c))]
+print("Zeroed 2D list, ",zrs)
+
+# the core code of the script
+for ss in range(len(s)):
+    if s[ss] != 0:
+        for ds in range(len(d)):
+            if d[ds] != 0:
+                zrs[ss][ds] = min(s[ss], d[ds])
+                s[ss] = s[ss] - zrs[ss][ds]    #update supply after alloc
+                d[ds] = d[ds] - zrs[ss][ds]    #update demand after alloc
+
+print("Alloc matrix with NWCM, ", zrs)
+
+sumz = 0
+for row in zrs:
+    sumz = sumz + sum(row)
+
+print("Sum of decision variables checks the sum of supply & demand, ", sumz == sum_s,",", sumz == sum_d)
 
 
