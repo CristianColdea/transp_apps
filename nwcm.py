94ca8da750b7f4240b4ec6f6b341a221af5f7f16
Cristian Coldea <rdt333@gmail.com>
2025-11-17 09:22:57 +0200

    Started coding the NW allocation method

---
+++ nwcm.py
@@ -0,0 +1,9 @@
+"""
+Script to determine a basic feasible solution (BFS) with North_West Corner
+allocation method.
+It takes as inputs the cost matrix, the supply and demand lists. The length of
+supply must match the number of cost matrix rows and that of demand the
+columns, respectively.
+Returns the allocation table, wether the feasible solution is possible, i.e.,
+bool and the allocation total cost.
+"""
