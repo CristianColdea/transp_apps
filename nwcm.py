2ce59912aa9e7a25b1a9752397f2d3598c519dd1
Cristian <rdt333@gmail.com>
2025-11-17 19:02:24 +0200

    continuing coding NWCM

--- nwcm.py
+++ nwcm.py
@@ -7,3 +7,18 @@ columns, respectively.
 Returns the allocation table, wether the feasible solution is possible, i.e.,
 bool, and the allocation total cost.
 """
+
+# Enter here the cost matrix
+# and supply and demand quantities
+c = [[2, 5, 1],
+     [7, 3, 2],
+     [1, 5, 3]]
+
+s = [25, 23, 26]
+d = [31, 22, 21]
+
+assert len(c) == len(s)   #rows must be = to number of supply sources
+assert len(c[0]) == len(d)    #cols must be = to number of demands
+assert sum(s) == sum(d)    #sum of supply must be = to sum of demand
+
+
