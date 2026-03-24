65d37c4ca402953878128bb3f7644a4e6cbb567c
Cristian Coldea <rdt333@gmail.com>
2026-03-24 19:12:04 +0200

    Minor update

--- magyar.py
+++ magyar.py
@@ -139,6 +139,7 @@ not_allocated = True
 
 (1, 3, 4), (1, 2, 5), (3, 2, 6)
 (3, 2, 5), (5, 5, 3), (5, 4, 2)
+(7, 6, 4, 3), (9, 5, 2, 6), (4, 8, 5, 3), (6, 2, 5, 8)
 
 while (not_allocated):
     c_work = c_red #get a working copy of reduced costs array
